d85ab131116ede92196d8f94d63c0119a8c70a50
Porter <pdhakes25@gmail.com>
2026-08-08 13:26:07 -0600

    created LEAGUES array for home page accordian and fetch ids

---
+++ src/utils/leagues.js
@@ -0,0 +1,19 @@
+export const LEAGUES = [
+  // Football
+  { id: 4391, name: "NFL", sport: "Football", level: "Professional" },
+  { id: 4479, name: "NCAA Division I Football", sport: "Football", level: "Collegiate" },
+
+  // Basketball
+  { id: 4387, name: "NBA", sport: "Basketball", level: "Professional" },
+  { id: 4516, name: "WNBA", sport: "Basketball", level: "Professional" },
+  { id: 4607, name: "NCAA Division I Basketball (M)", sport: "Basketball", level: "Collegiate" },
+
+  // Baseball
+  { id: 4424, name: "MLB", sport: "Baseball", level: "Professional" },
+
+  // Hockey
+  { id: 4380, name: "NHL", sport: "Hockey", level: "Professional" },
+
+  // Soccer
+  { id: 4684, name: "USL Championship", sport: "Soccer", level: "Professional" },
+];

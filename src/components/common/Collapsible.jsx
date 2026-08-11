06e19e568eb3275c05bb727f7fb716869fa5c420
Porter <pdhakes25@gmail.com>
2026-08-10 18:43:14 -0600

    added missing id to collapsible wrapper

--- src/components/common/Collapsible.jsx
+++ src/components/common/Collapsible.jsx
@@ -1,7 +1,7 @@
 
-function Collapsible({ isOpen, children }) {
+function Collapsible({ id, isOpen, children }) {
     return (
-        <div className={isOpen ? "collapsible collapsible--open" : "collapsible"}>
+        <div id={id} className={isOpen ? "collapsible collapsible--open" : "collapsible"}>
             <div className="collapsible__inner">
             {children}
             </div>

79ac28b63ad18c7d4b0827233eebba1217baac4f
Porter <pdhakes25@gmail.com>
2026-08-15 12:24:54 -0600

    implemented PlayerCard with passing tests

--- src/components/team/PlayerCard.jsx
+++ src/components/team/PlayerCard.jsx
@@ -1,6 +1,19 @@
-function PlayerCard () {
+import { Link } from "react-router-dom"
+
+function PlayerCard ({ player }) {
     return (
-        <h3>Player</h3>
+        <Link to={`/player/${player.idPlayer}`}>
+            {player?.strCutout && (
+                <img src={player.strCutout} alt={`${player.strPlayer} cutout`} />
+            )}
+            <h4>{player.strPlayer}</h4>
+            {player?.strPosition && (
+                <p>{player.strPosition}</p>
+            )}
+            {player?.strNationality && (
+                <p>{player.strNationality}</p>
+            )}
+        </Link>
     )
 }
 

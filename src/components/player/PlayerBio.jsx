6e66852758cf58b80eadfe5730251128bee8479a
Porter <pdhakes25@gmail.com>
2026-08-21 19:29:12 -0600

    implemented PlayerBio with passing tests

--- src/components/player/PlayerBio.jsx
+++ src/components/player/PlayerBio.jsx
@@ -1,8 +1,36 @@
-function PlayerBio() {
+import { useState } from "react"
+import Collapsible from "../common/Collapsible"
+
+function PlayerBio({ player }) {
+    const [descriptionOpen, setDescriptionOpen] = useState(true)
+
     return(
-        <>
-            <h1>BIO</h1>
-        </>
+        <div className="player-bio">
+            {player.dateBorn && (
+                <p>Born {player.dateBorn}</p>
+            )}
+
+            {player.strBirthLocation && (
+                <p>{player.strBirthLocation}</p>
+            )}
+
+            {player.strNationality && (
+                <p>{player.strNationality}</p>
+            )}
+
+            {(player.strHeight || player.strWeight) && (
+                <p>{player.strHeight} {player.strWeight}</p>
+            )}
+
+            {player.strDescriptionEN && (
+                <>
+                    <button onClick={() => setDescriptionOpen(prev => !prev)}>{descriptionOpen ? "Hide" : "Show"} description</button>
+                    <Collapsible isOpen={descriptionOpen}>
+                        <p>{player.strDescriptionEN}</p>
+                    </Collapsible>
+                </>
+            )}
+        </div>
     )
 }
 

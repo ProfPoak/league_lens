42c34dbb46dfa6abe9b7babc17fd74263d0f8984
Porter <pdhakes25@gmail.com>
2026-08-22 16:48:35 -0600

    added comments for groupBy util

--- src/utils/roster.js
+++ src/utils/roster.js
@@ -1,3 +1,6 @@
+//returns an object with items grouped into arrays.
+//keyFn requires a callback to corelate the key and the item.
+
 export function groupBy(items, keyFn) {
   return items.reduce((acc, item) => {
     const key = keyFn(item);

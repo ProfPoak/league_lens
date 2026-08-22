53f44fcad2974327647067ff0b0fe2ae7c66d04e
Porter <pdhakes25@gmail.com>
2026-08-21 19:14:23 -0600

    implemented PlayerHeader with passing tests

--- src/components/player/PlayerHeader.jsx
+++ src/components/player/PlayerHeader.jsx
@@ -1,7 +1,24 @@
-function PlayerHeader() {
+import { Link } from "react-router-dom"
+
+function PlayerHeader({ player }) {
     return(
         <>
-            <h1>HEADER</h1>
+            {(player.strCutout || player.strThumb) && (
+                <img src={player.strCutout || player.strThumb} alt={`${player.strPlayer}`} />
+            )}
+            <h1>{player.strPlayer}</h1>
+
+            {player.strTeam && player.idTeam &&(
+                <Link to={`/team/${player.idTeam}`}>{player.strTeam}</Link>
+            )}
+
+            {player.strPosition && (
+                <span>{player.strPosition}</span>
+            )}
+
+            {player.strNumber && (
+                <span>#{player.strNumber}</span>
+            )}
         </>
     )
 }

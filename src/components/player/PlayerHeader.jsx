c1fbf8fbf680e56bffc2ea46bc651d44ed4aa220
Porter <pdhakes25@gmail.com>
2026-08-22 08:58:00 -0600

    implemented styling and added missing classNames to PlayerHeader

--- src/components/player/PlayerHeader.jsx
+++ src/components/player/PlayerHeader.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom"
 
 function PlayerHeader({ player }) {
+    
     return(
         <div className="player-header">
             {(player.strCutout || player.strThumb) && (
                 <img className="player-header__photo" src={player.strCutout || player.strThumb} alt={`${player.strPlayer}`} />
             )}
-            <div>
-                <h1>{player.strPlayer}</h1>
+            <div className="player-header__info">
+                <h1 className="player-header__name">{player.strPlayer}</h1>
 
-                {player.strTeam && player.idTeam &&(
-                    <Link to={`/team/${player.idTeam}`}>{player.strTeam}</Link>
+                {player.strTeam && player.idTeam && (
+                    <Link className="player-header__team" to={`/team/${player.idTeam}`}>{player.strTeam}</Link>
                 )}
-                
+
                 <div className="player-header__meta">
                     {player.strPosition && (
                         <span className="player-header__badge">{player.strPosition}</span>
